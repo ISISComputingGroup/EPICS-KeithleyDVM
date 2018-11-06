2726efb249fd2e722544d7aaa47c3cfcb6b792b0
Liam <02liamp@gmail.com>
2018-11-06 10:48:32 +0000

    small change to parse script

--- Keithley_2700Sup/parse_buffer_readings.c
+++ Keithley_2700Sup/parse_buffer_readings.c
@@ -8,8 +8,8 @@
 /*
 *	Keithley 2700 Resistance Buffer Readings Parser
 *
-* 	Takes data from aSub record BUFF:READ, finds the Reading and Timestamp value and 
-*	puts it into the correct Channel. 
+* 	Takes data from aSub record BUFF:READ, finds the Reading and Timestamp value and
+*	puts it into the correct Channel.
 *
 *	VALA (value A) = channel 101 reading = CHNL:101:READ etc
 *
@@ -24,30 +24,30 @@ static void assign_value_to_pv(double *val, epicsEnum16 ftv, double reading) {
 		((double *)val)[0] = reading;
 	}
 }
-	
+
 static long parse_buffer_readings(aSubRecord *prec, long array_offset) {
-	
-	/* 	'array_offset' refers to the index of the buffer array. This value will be 0 or 1, allowing 
+
+	/* 	'array_offset' refers to the index of the buffer array. This value will be 0 or 1, allowing
 	* 	this script to parse the timestamp and reading value depending on the value of array_offset.
 	* 	a[i] = channel reading
 	* 	a[i+1] = channel timestamp
-	* 	a[i+2] = channel 
+	* 	a[i+2] = channel
 	*/
-	
+
 	double *a;
 	// We have 3 values per channel - reading, timestamp and channel
 	const int channel_values_count = 3;
 	long i = 0;
 	// prec = INPA from keithley2700.db, BUFF:READ - a waveform PV
     prec->pact = 1;
-	
-    a = (double *)prec->a;	
-	
+
+    a = (double *)prec->a;
+
 	while((a[i] > 0) && (i < (prec->noa)-channel_values_count)) {
 
-		int channel = (int)(a[i+2]+0.5); // protection against double to int rounding errors		
+		int channel = (int)(a[i+2]+0.5); // protection against double to int rounding errors
 		double value = a[i+array_offset];
-		
+
 		// Find the channel and add reading value to correct channel PV
 		switch(channel) {
 			case 101:
@@ -110,11 +110,11 @@ static long parse_buffer_readings(aSubRecord *prec, long array_offset) {
 			case 210:
 				assign_value_to_pv(prec->valt, prec->ftva, value);
 				break;
-			default: 
-				printf("\n>>No Channel Found for value: %f", value);
+			default:
+				printf("\n>>No Channel Found for value: %f (channel %d)", value, channel);
 				i=i+1;
 				break;
-		}		
+		}
 		i=i+channel_values_count;
     }
 
